b5aa7fdf3e554b8d3974d93cfd7653b6dd6c1e59
hongyichao <lucaschao@outlook.com>
2015-08-11 22:25:09 +1000

    add project web API controller

---
+++ TaskScheduler/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ProjectController : ApiController
+    {        
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IQueryable GetItems() {
+            return db.Projects;
+        }
+
+
+    }
+}
